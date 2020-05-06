19e9b2abdf1f4473a1b65f14b6e7fe6ad6ed2fe6
Pedro Cervantes <pacr980612@gmail.com>
2020-05-05 21:55:19 -0500

    first commit

---
+++ MARVEL PEDRO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MARVEL PEDRO
+//
+//  Created by Pedro Cervantes on 05/05/20.
+//  Copyright © 2020 Pedro Cervantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
